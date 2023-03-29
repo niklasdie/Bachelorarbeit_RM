438e1bdc79e9102fa1e0604b3f5fc73c61368787
Niklas Diekhöner <niklasdiekhoener@gmail.com>
2023-03-29 10:48:31 +0200

    fixed receiver ip

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     udp_sender client(s,argv[2], port);
     std::cout << "Start sending" << std::endl;
     for (int i = 0; i < 21; i++) {
-        std::cout  << " \033[0;34mSM: \033[0m" << s.get_data() << std::endl;
+        std::cout  << "\033[0;34mSM: \033[0m" << s.get_data() << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(150));
         s.set_data(new char('0' + (i % 10)), i);
         client.send_data(0);
